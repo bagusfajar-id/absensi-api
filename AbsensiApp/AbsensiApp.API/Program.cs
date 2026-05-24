4d5b8ed5fd2039616a23d468920a5900383d9973
bagusfajar-id <bagusfajar.it@gmail.com>
2026-05-24 20:58:23 +0700

    program cs

--- AbsensiApp/AbsensiApp.API/Program.cs
+++ AbsensiApp/AbsensiApp.API/Program.cs
@@ -103,11 +103,8 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowAll");
 app.UseHttpsRedirection();
